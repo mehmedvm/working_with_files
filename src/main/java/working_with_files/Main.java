0c2962b69896b19cc2e120b479599fd024d0ebdb
MehmedVM <mehmedvm@gmail.com>
2022-04-01 12:56:18 -0400

    initial files

---
+++ src/main/java/working_with_files/Main.java
@@ -0,0 +1,17 @@
+package working_with_files;
+
+public class Main {
+    public static void main(String[] args) {
+        String welcome = "Welcome to Files";
+        System.out.println(welcome);
+
+        /* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+         */
+
+
+    }
+
+
+
+}
